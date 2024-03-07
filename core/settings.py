06d5e9cd036b6359b259c0fd8b8fec63d51c1721
Ali <aliahadmd1@gmail.com>
2024-03-07 18:58:22 +0600

    add postgresql

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,18 @@ WSGI_APPLICATION = "core.wsgi.app"
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
 
-DATABASES = { "default": { "ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3"), } }
+# DATABASES = { "default": { "ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3"), } }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
 # Password vimagedation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-vimagedators
